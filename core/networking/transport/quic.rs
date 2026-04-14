955f7bb0a28916e7baeddd11d810fa7b862ae84f
Omega <emomega@proton.me>
2026-04-13 22:56:07 -0400

    Create quic.rs

---
+++ core/networking/transport/quic.rs
@@ -0,0 +1,9 @@
+// Placeholder QUIC implementation (use quinn or similar crate later)
+
+pub fn send_quic(_addr: &str, _data: &[u8]) {
+    println!("QUIC send (stub)");
+}
+
+pub fn receive_quic() {
+    println!("QUIC receive (stub)");
+}
