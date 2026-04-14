99b77878c77c1e580f83f215d9e0f5d70057f426
Omega <emomega@proton.me>
2026-04-13 23:34:20 -0400

    Create path2.rs

---
+++ pipelines/path2.rs
@@ -0,0 +1,26 @@
+use crate::pipelines::builder::{Pipeline, PipelineStage};
+
+struct DummyStage(&'static str);
+
+impl PipelineStage for DummyStage {
+    fn process(&self, data: Vec<u8>) -> Vec<u8> {
+        println!("[PIPELINE] Passing through {}", self.0);
+        data
+    }
+}
+
+pub fn build_path2() -> Pipeline {
+    let mut pipeline = Pipeline::new();
+
+    pipeline.add_stage(DummyStage("Shadowsocks"));
+    pipeline.add_stage(DummyStage("Firewall"));
+    pipeline.add_stage(DummyStage("VPN"));
+    pipeline.add_stage(DummyStage("Soft Layer"));
+    pipeline.add_stage(DummyStage("Tor"));
+    pipeline.add_stage(DummyStage("Meek"));
+    pipeline.add_stage(DummyStage("Firewall"));
+    pipeline.add_stage(DummyStage("VPN"));
+    pipeline.add_stage(DummyStage("DNS/DSCP"));
+
+    pipeline
+}
